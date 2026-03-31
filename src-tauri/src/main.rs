182f43a8df38cab27698f0829e263014b280b99c
Martin Alvis <martin.alvis@sonos.com>
2026-03-31 20:23:30 +0100

    feat: close window to background instead of quitting

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -251,6 +251,12 @@ fn main() {
 
             Ok(())
         })
+        .on_window_event(|window, event| {
+            if let tauri::WindowEvent::CloseRequested { api, .. } = event {
+                window.hide().unwrap();
+                api.prevent_close();
+            }
+        })
         .run(tauri::generate_context!())
         .expect("error running Keybow Companion");
 }
